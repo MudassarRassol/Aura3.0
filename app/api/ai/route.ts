94d5b1a4b954c4761da9ccd5583a7d63f14e7f69
Mudassar Rasool <mudassardev16@gmail.com>
2025-10-15 19:28:53 +0500

    Update route.ts

--- app/api/ai/route.ts
+++ app/api/ai/route.ts
@@ -110,7 +110,7 @@ export async function GET(req: Request) {
     `;
 
     const aiResponse = await generateText({
-      model: google("gemini-pro")
+      model: google("gemini-pro"),
       prompt: aiPrompt,
       maxOutputTokens: 100,
     });
